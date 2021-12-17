8cce7c8b3d75cc536fef7ec59dcbbd2a7d30dab5
keemdedone <komchan447@gmail.com>
2021-12-17 12:31:35 +0700

    complete requirement

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -5,11 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './status.component.html',
   styleUrls: ['./status.component.scss']
 })
-export class StatusComponent implements OnInit {
-
-  constructor() { }
+export class StatusComponent implements OnInit{
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit(): void {
-    }
+    const date1 = new Date('December 17, 1995 03:24:00');
+  }
+
+  cookie = navigator.cookieEnabled;
+  language = navigator.languages;
+  vendor = navigator.vendor;
+  maxTouch = navigator.maxTouchPoints;
+  userAgent = navigator.userAgent;
+  processor = navigator.hardwareConcurrency;
+  timeStamp = Date();
   }
